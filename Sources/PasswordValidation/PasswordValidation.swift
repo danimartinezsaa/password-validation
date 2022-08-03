b608121ccf11e6be3773366c659a9a6f2e52abf2
Dani Martínez <daniel.martinez@quobis.com>
2022-08-03 16:03:52 +0200

    Iteration 1 - Basic password validation

--- Sources/PasswordValidation/PasswordValidation.swift
+++ Sources/PasswordValidation/PasswordValidation.swift
@@ -1,6 +1,20 @@
+import Foundation
 public struct PasswordValidation {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
+    
+    private let MIN_LENGHT = 8
+    
+    func validate(_ password: String) -> Bool {
+        
+        return containsAtLeastOneLowercase(password) &&
+            containsAtLeastOneUppercase(password) &&
+            hasTheMinimunLenght(password) &&
+            containsAtLeastOneNumber(password) &&
+            containsAtLeastOneUnderscore(password)
     }
+    
+    private func containsAtLeastOneUppercase(_ text: String) -> Bool { text.lowercased() != text }
+    private func containsAtLeastOneLowercase(_ text: String) -> Bool { text.uppercased() != text }
+    private func hasTheMinimunLenght(_ text: String) -> Bool { text.count >= MIN_LENGHT }
+    private func containsAtLeastOneNumber(_ text: String) -> Bool { text.rangeOfCharacter(from: CharacterSet.decimalDigits) != nil }
+    private func containsAtLeastOneUnderscore(_ text: String) -> Bool { text.contains("_") }
 }
--- Tests/PasswordValidationTests/PasswordValidationTests.swift
+++ Tests/PasswordValidationTests/PasswordValidationTests.swift
@@ -2,10 +2,40 @@ import XCTest
 @testable import PasswordValidation
 
 final class PasswordValidationTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(PasswordValidation().text, "Hello, World!")
+    
+    private var sut: PasswordValidation!
+    
+    override func setUpWithError() throws {
+        sut = PasswordValidation()
+    }
+
+    func testShouldValidateValidPassword() {
+        let isValid = sut.validate("Passw0rd_")
+        XCTAssertTrue(isValid)
+    }
+    
+    func testPasswordWithLessThan8CharactersShouldBeInvalid() throws {
+        let isValid = sut.validate("pass0_")
+        XCTAssertFalse(isValid)
+    }
+    
+    func testPasswordWithoutCapitalLettersShouldBeInvalid() throws {
+        let isValid = sut.validate("passw0rd_")
+        XCTAssertFalse(isValid)
+    }
+    
+    func testPasswordWithoutAtLeastOneLowecaseShouldBeInvalid() throws {
+        let isValid = sut.validate("PASSW0RD_")
+        XCTAssertFalse(isValid)
+    }
+    
+    func testPasswordWithoutNumbersShouldBeInvalid() throws {
+        let isValid = sut.validate("Password_")
+        XCTAssertFalse(isValid)
+    }
+    
+    func testPasswordWithoutUnderscoresShouldBeInvalid() throws {
+        let isValid = sut.validate("Passw0rd")
+        XCTAssertFalse(isValid)
     }
 }
